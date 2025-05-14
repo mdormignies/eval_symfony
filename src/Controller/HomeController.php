5c4314edafaab9f4d2b99a2e937dfef470a1f426
mdormignies <dormignies.maxime@gmail.com>
2025-05-14 13:23:24 +0200

    oui

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        if ($this->getUser()) {
+            return $this->render('home/index.html.twig');
+        }
+
+        return $this->redirectToRoute('app_login');
+    }
+}
